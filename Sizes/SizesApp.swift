609816c7b77539e578d410715c6ccb039499992e
gyaltsentenzin <40735388+gyaltsentenzin@users.noreply.github.com>
2024-05-16 23:08:47 -0500

    Initial Commit

---
+++ Sizes/SizesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SizesApp.swift
+//  Sizes
+//
+//  Created by Tenzin Gyaltsen on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SizesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
